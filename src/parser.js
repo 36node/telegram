9490f4402b0c65a47415c5f39cd44a6f3b3d245a
yaoxiaoqi <yaoxiaoqi123@gmail.com>
2018-08-16 18:52:08 +0800

    feat: string can be array item

--- src/parser.js
+++ src/parser.js
@@ -68,10 +68,7 @@ export default class Telegram {
     if (!options.type) {
       throw new Error("Type option of array is not defined.");
     }
-    if (
-      typeof options.type === "string" &&
-      !(has.call(PRIMITIVE_TYPES, NAME_MAP[options.type]) || options.type === "string")
-    ) {
+    if (typeof options.type === "string" && !(has.call(PRIMITIVE_TYPES, NAME_MAP[options.type]) || options.type === "string")) {
       throw new Error(`Specified primitive type ${options.type} is not supported.`);
     }
 
@@ -288,7 +285,7 @@ class bits extends Processor {
   realParseBit() {
     const { buffer, offset, bitOffset } = this.buf;
     const {
-      options: { length },
+      options: { length }
     } = this.bitItem;
     const byteToBeRead = Math.ceil((bitOffset + length) / 8);
     let tmp = 0;
@@ -328,7 +325,7 @@ class bits extends Processor {
   updateStatus() {
     const { bitOffset } = this.buf;
     const {
-      options: { length },
+      options: { length }
     } = this.bitItem;
     this.buf.bitOffset = (bitOffset + length) % 8;
     const carry = Math.floor((bitOffset + length) / 8);
@@ -339,7 +336,7 @@ class bits extends Processor {
 class nest extends Processor {
   realParse() {
     const {
-      options: { type },
+      options: { type }
     } = this.item;
     if (type instanceof Telegram) {
       const { result: new_result } = type.parse(this.buf, {});
@@ -372,7 +369,7 @@ class array extends Processor {
 
   defineType() {
     const {
-      options: { type },
+      options: { type }
     } = this.item;
     if (typeof type === "string") {
       if (Object.keys(PRIMITIVE_TYPES).includes(NAME_MAP[type])) {
@@ -390,7 +387,7 @@ class array extends Processor {
   realParse() {
     let i = 0;
     const {
-      options: { length },
+      options: { length }
     } = this.item;
     const arrayLength = typeof length === "number" ? length : this.result[length];
     for (i = 0; i < arrayLength; i++) {
@@ -412,7 +409,7 @@ class array extends Processor {
         break;
       case "string":
         const {
-          options: { subOptions },
+          options: { subOptions }
         } = this.item;
         const stringParser = new Telegram().string("tmp", subOptions);
         const { result: str_result } = stringParser.parse(this.buf, {});
@@ -445,7 +442,7 @@ class skip extends Processor {
 
   updateStatus() {
     const {
-      options: { length },
+      options: { length }
     } = this.item;
     this.buf.offset += length;
   }
@@ -460,7 +457,7 @@ class string extends Processor {
 
   realParse() {
     const {
-      options: { length, encoding, zeroTerminated, greedy, stripNull },
+      options: { length, encoding, zeroTerminated, greedy, stripNull }
     } = this.item;
     const { buffer, offset } = this.buf;
 
