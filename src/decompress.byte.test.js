82b1b054e92b605e5889d0feeffbeb907565b1fd
lidongbest5 <lidongbest5@gmail.com>
2020-12-10 11:25:15 +0800

    feat: add byte decompress mode (#17)
    * feat: add byte decompress mode
    
    * feat: add decompress byte test
    
    * fix: update offset name to length

---
+++ src/decompress.byte.test.js
@@ -0,0 +1,63 @@
+import Telegram from "./parser";
+
+// 测试decompress下byte位置
+test("should decompress tcp header buffer with right byte position", () => {
+  /**
+   * @type {Telegram}
+   */
+  const tcpHeader = new Telegram()
+    .endianess("big")
+    .uint16("srcPort")
+    .uint16("dstPort")
+    .uint32("seq")
+    .uint32("ack")
+    .uint16("windowSize")
+    .uint16("checksum")
+    .uint16("urgentPointer");
+
+  const buffer = Buffer.from("e8a203e108e177e13d20756b29d300410000", "hex");
+
+  const obj = {
+    length: 18,
+    result: {
+      ack: {
+        length: 4,
+        start: 8,
+        value: 1025537387,
+      },
+      checksum: {
+        length: 2,
+        start: 14,
+        value: 65,
+      },
+      dstPort: {
+        length: 2,
+        start: 2,
+        value: 993,
+      },
+      seq: {
+        length: 4,
+        start: 4,
+        value: 148994017,
+      },
+      srcPort: {
+        length: 2,
+        start: 0,
+        value: 59554,
+      },
+      urgentPointer: {
+        length: 2,
+        start: 16,
+        value: 0,
+      },
+      windowSize: {
+        length: 2,
+        start: 12,
+        value: 10707,
+      },
+    },
+  };
+
+  const decompressed = tcpHeader.decompress(buffer, true, true);
+  expect(decompressed).toEqual(obj);
+});
